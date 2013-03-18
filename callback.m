1785bbc6d5bd4bb264757e78beea6b679133f735
Ian Joynes <ian@server.fake>
2013-03-18 15:38:07 -0400

    Revert "Code clean up"
    This reverts commit 7d3bc01d808e675d731f1148350572b765231786.

--- callback.m
+++ callback.m
@@ -1,67 +1,37 @@
-function [stop,user_data] = callback(x,iter,state,user_data,opts)
+function [stop,user_data] = test_callback(x,iter,state,user_data,opts)
 
-global inputDir simDir c_k c_star m m_star f f_obs df df_obs s x
+global inputDir simDir c_k c_star m m_star
 
 switch state
   case 'init'
     fprintf('------------------------------------------------------------------------------------------------------------\n');
     fprintf('|       Date/Time      | Iter |      f       |  inf_norm(g)  |      r^2      |      r       |     m/m*     |\n');
     fprintf('------------------------------------------------------------------------------------------------------------\n');
-    nx = length(x);
-    niters = opts.maxits + 1;
-    
-    user_data.m_star = m_star;
-    user_data.its = 0;
-    user_data.f = zeros(1, niters);
-    user_data.f_obs = zeros(1, niters);
-    user_data.r  = zeros(1, niters);
-    user_data.r2 = zeros(1, niters);
-    user_data.m = zeros(1, niters);
 
-    user_data.x = zeros(nx, niters);
-    user_data.s = zeros(nx, niters);
-    user_data.g = zeros(nx, niters);
-    user_data.g_obs = zeros(nx, niters);
-    
+    user_data.f = zeros(1,opts.maxits);
+    user_data.x = zeros(length(x),opts.maxits);
+    user_data.its = 0;
     
   case 'iter'
+    user_data.f(iter.it) = iter.f;
+    user_data.x(:,iter.it) = x;
+    user_data.its = user_data.its + 1;
 
-    if iter.it == 1
-        user_data.f(1) = f_0;
-        user_data.f_obs(1) = f_obs_0;
-        user_data.r(1) = r;
-        user_data.r(1) = r2;
-        user_data.m(1) = m;
-        user_data.m_norm(1) = m/m_star;
-        user_data.x
-    end
+    file_num = generate_file_num(nPass, nt);
+    results_path = fullfile(iter_dir, [iter_label, file_num, '.mat']);
 
     s = volumetric_emission_rate(x);
     c = c_k;
     r  = compute_correlation_coefficient(c_k, c_star);
     r2 = compute_coefficient_of_determination(c_k, c_star);
-    m_norm = m/m_star;
 
-    user_data.f(iter.it+1) = iter.f;
-    user_data.f_obs(iter.it+1) = f_obs;
-    user_data.r(iter.it+1) = r;
-    user_data.r2(iter.it+1) = r2;
-
-
-    user_data.x(:,iter.it+1) = x;
-    user_data.its = user_data.its + 1;
-
-    
     %m = dot(spaceIntWeight, E_approx);
-
+    m_norm = m/m_star;
 
     fprintf('| %s | %4d | %8.6e |  %8.6e |  %9.6e | %8.6e | %8.6e |\n', datestr(now), iter.it, iter.f, max(abs(iter.g)),r2,r,m_norm);
     s = x;
     f = iter.f;
     g = iter.g;
-
-    file_num = generate_file_num(nPass, nt);
-    results_path = fullfile(iter_dir, [iter_label, file_num, '.mat']);
     save(results_path,'f','f_obs', 'df', 'df_obs', 's', 'x' 'c','r','r2','m','m_norm');
 
   case 'done'
